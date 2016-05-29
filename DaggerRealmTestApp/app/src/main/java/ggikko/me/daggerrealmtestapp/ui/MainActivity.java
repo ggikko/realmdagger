206c65ca313a6253cbd05e66d2883bebaf550db3
Jihong Park <ggikko@hotmail.com>
2016-05-29 12:30:38 +0900

    dependnecy

--- DaggerRealmTestApp/app/src/main/java/ggikko/me/daggerrealmtestapp/ui/MainActivity.java
+++ DaggerRealmTestApp/app/src/main/java/ggikko/me/daggerrealmtestapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package ggikko.me.daggerrealmtestapp;
+package ggikko.me.daggerrealmtestapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ggikko.me.daggerrealmtestapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
